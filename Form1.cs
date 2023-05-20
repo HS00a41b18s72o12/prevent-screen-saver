19bf72cf034ace4f25afb2e0d6048c477e50b428
HS00a41b18s72o12 <a16a1x1440000@gmail.com>
2023-05-20 09:13:10 +0900

    マウスカーソルを物理的に動かすように改修

--- Form1.cs
+++ Form1.cs
@@ -139,7 +139,7 @@ namespace preventScreenSaver
             {
                 File.WriteAllText(RUNNING_CHECK_FILE_PATH, length.ToString());
             }
-            
+
             ScreenTimer(length);
         }
         private void button2_Click(object sender, EventArgs e)
@@ -222,10 +222,32 @@ namespace preventScreenSaver
             DateTime endTime = DateTime.Now.AddMinutes(minutes);
             this.label1.Text = "0 / " + minutes;
             Application.DoEvents();
+            bool cursorVector = false;
+            int previousCurrentMinute = 0;
             while (true)
             {
                 Thread.Sleep(200);
-                PreventScreenSaverFromStarting();
+
+                // 1分毎にマウスカーソルを動かす
+                DateTime now = DateTime.Now;
+                int currentMinute = now.Minute;
+                if (previousCurrentMinute != currentMinute)
+                {
+                    var pt = GetCursor.GetCursorPosition();
+                    // true false 反転、交互にマウスカーソルを動かす位置を変える
+                    if (cursorVector)
+                    {
+                        cursorVector = false;
+                        SetCursor.SetCursorPos(pt.X, pt.Y + 1);
+                    }
+                    else
+                    {
+                        cursorVector = true;
+                        SetCursor.SetCursorPos(pt.X, pt.Y - 1);
+                    }
+                }
+                previousCurrentMinute = currentMinute;
+
                 string _endTime = endTime.ToString("ddHHmm");
                 string _currentTime = DateTime.Now.ToString("ddHHmm");
                 if (_endTime == _currentTime)
@@ -240,82 +262,38 @@ namespace preventScreenSaver
             }
             this.label1.Text = this.label1.Text + " 終了";
         }
-        static void PreventScreenSaverFromStarting()
-        {
-            INPUT input = new INPUT();
-            input.type = INPUT_MOUSE;
-            input.mi = new MOUSEINPUT();
 
-            input.mi.dwExtraInfo = IntPtr.Zero;
-            input.mi.dx = 0;
-            input.mi.dy = 0;
-            input.mi.time = 0;
-            input.mi.mouseData = 0;
-            input.mi.dwFlags = 0x0001; // MOVE (RELATIVE)
-            int cbSize = Marshal.SizeOf(typeof(INPUT));
-            uint r = SendInput(1, ref input, cbSize);
+
+        public static class SetCursor
+        {
+            [DllImport("user32.dll")]
+            public static extern bool SetCursorPos(int X, int Y);
         }
 
-        #region Win32 API
-        [StructLayout(LayoutKind.Sequential)]
-        struct MOUSEINPUT
+        public static class GetCursor
         {
-            public int dx;
-            public int dy;
-            public uint mouseData;
-            public uint dwFlags;
-            public uint time;
-            public IntPtr dwExtraInfo;
+            #region Win32API
+            [DllImport("User32.dll")]
+            static extern bool GetCursorPos(out POINT lppoint);
+            [StructLayout(LayoutKind.Sequential)]
+            struct POINT
+            {
+                public int X { get; set; }
+                public int Y { get; set; }
+                public static implicit operator Point(POINT point)
+                {
+                    return new Point(point.X, point.Y);
+                }
+            }
+            #endregion
+
+            public static Point GetCursorPosition()
+            {
+                var pt = new POINT();
+                GetCursorPos(out pt);
+                return pt;
+            }
         }
-        const int INPUT_MOUSE = 0;
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct KEYBDINPUT
-        {
-            ushort wVk;
-            ushort wScan;
-            uint dwFlags;
-            uint time;
-            IntPtr dwExtraInfo;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct HARDWAREINPUT
-        {
-            uint uMsg;
-            ushort wParamL;
-            ushort wParamH;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        struct INPUT
-        {
-            [FieldOffset(0)]
-            public int type;
-            [FieldOffset(4)] //*
-            public MOUSEINPUT mi;
-            [FieldOffset(4)] //*
-            public KEYBDINPUT ki;
-            [FieldOffset(4)] //*
-            public HARDWAREINPUT hi;
-        }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
-
-        [FlagsAttribute]
-        public enum EXECUTION_STATE : uint
-        {
-            ES_SYSTEM_REQUIRED = 0x00000001,
-            ES_DISPLAY_REQUIRED = 0x00000002,
-            // Legacy flag, should not be used.
-            // ES_USER_PRESENT   = 0x00000004,
-            ES_CONTINUOUS = 0x80000000,
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
-        #endregion
 
         private void label1_Click(object sender, EventArgs e)
         {
